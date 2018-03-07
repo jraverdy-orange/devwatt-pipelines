0b8903cc3a8e8933493df07cf46a68b40f3e593c
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-03-07 12:25:56 +0100

    deploy without prompting confirmation

--- ci/prometheus-upgrade/upgrade-prometheus/scripts/upgrade-prometheus.sh
+++ ci/prometheus-upgrade/upgrade-prometheus/scripts/upgrade-prometheus.sh
@@ -6,11 +6,12 @@ export ROOT_FOLDER=${PWD}
 
 export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
 
-bosh -e ${ALIAS} -d prometheus deploy prometheus-boshrelease/manifests/prometheus.yml \
+
+bosh -e ${ALIAS} -d prometheus deploy -n prometheus-boshrelease/manifests/prometheus.yml \
   -o ${ROOT_FOLDER}/devwatt-pipelines/opsfiles/prometheus-opsfile.yml \
   -o prometheus-boshrelease/manifests/operators/monitor-bosh.yml \
   -o prometheus-boshrelease/manifests/operators/enable-bosh-uaa.yml \
   -v bosh_url=${IP} \
   -v uaa_bosh_exporter_client_secret=${UAA_BOSH_EXPORTER_CLIENT_SECRET} \
-  -v bosh_ca_cert=<(echo ${BOSH_CA}) \
+  --var-file bosh_ca_cert=${ROOT_FOLDER}/bosh-director-config/bosh_ca.crt \
   -v metrics_environment=${METRICS_ENVIRONMENT}

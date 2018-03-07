d040286b05dac20232e214e3b3ee75295d4c6012
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-03-07 12:20:31 +0100

    Bad opsfile path

--- ci/prometheus-upgrade/upgrade-prometheus/scripts/upgrade-prometheus.sh
+++ ci/prometheus-upgrade/upgrade-prometheus/scripts/upgrade-prometheus.sh
@@ -7,10 +7,10 @@ export ROOT_FOLDER=${PWD}
 export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
 
 bosh -e ${ALIAS} -d prometheus deploy prometheus-boshrelease/manifests/prometheus.yml \
-  -o prometheus-opsfile.yml \
+  -o ${ROOT_FOLDER}/devwatt-pipelines/opsfiles/prometheus-opsfile.yml \
   -o prometheus-boshrelease/manifests/operators/monitor-bosh.yml \
   -o prometheus-boshrelease/manifests/operators/enable-bosh-uaa.yml \
   -v bosh_url=${IP} \
   -v uaa_bosh_exporter_client_secret=${UAA_BOSH_EXPORTER_CLIENT_SECRET} \
-  -v bosh_ca_cert=${BOSH_CA} \
+  -v bosh_ca_cert=<(echo ${BOSH_CA}) \
   -v metrics_environment=${METRICS_ENVIRONMENT}

8d1e88b148080df3c713e0d10f818af4476fcfe1
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2018-07-19 16:01:53 +0200

    Use the last bosh available version of postgres

--- ci/prometheus-upgrade/upgrade-prometheus/scripts/upgrade-prometheus.sh
+++ ci/prometheus-upgrade/upgrade-prometheus/scripts/upgrade-prometheus.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash 
 
-set -ex
+set -e
+
+# install curl
+apt update
+apt install -y curl
 
 export ROOT_FOLDER=${PWD}
 
 export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
-
-# forcing release... must to be fixed
-
 
 
 cd ${ROOT_FOLDER}/prometheus-boshrelease
@@ -16,8 +17,18 @@ last_tag=$(git for-each-ref --sort=-taggerdate --count=1 refs/tags|sed -e "s/.*\
 git checkout $last_tag
 cd ${ROOT_FOLDER}
 
+# downloading the needed release
+url=$(grep "https://github.com/bosh-prometheus/prometheus-boshrelease/releases/download" \
+			prometheus-boshrelease/manifests/prometheus.yml \
+	|awk '{print $2}')
+curl -o prometheus-release.tar.gz -L ${url}
+# uploading the release
+bosh -e ${ALIAS} upload-release prometheus-release.tar.gz
+
+
 bosh -e ${ALIAS} -d prometheus deploy -n prometheus-boshrelease/manifests/prometheus.yml \
   -o ${ROOT_FOLDER}/devwatt-pipelines/opsfiles/prometheus2-opsfile.yml \
+  -o ${ROOT_FOLDER}/devwatt-pipelines/opsfiles/use-last-postgres-release-opsfile.yml \
   -o prometheus-boshrelease/manifests/operators/monitor-bosh.yml \
   -o prometheus-boshrelease/manifests/operators/enable-bosh-uaa.yml \
   -v bosh_url=${IP} \
